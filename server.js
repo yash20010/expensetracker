9fe32f44e60f24bee38289505705bd4bcf3082f8
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-27 19:54:25 -0600

    Updated scripts

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/v1/transactions', transactions)
 
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 )
 
 const PORT = process.env.PORT || 5000
