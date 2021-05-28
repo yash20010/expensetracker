515b15712b22517a52c38464973084c7ba0a01ea
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-27 19:45:07 -0600

    Minor Updates

--- Procfile
+++
@@ -1,4 +0,0 @@
-"scripts": {
-    "start": "node server.js",
-    "heroku-postbuild": "cd client && npm install --only=dev && npm install && npm run build"
-}
--- server.js
+++ server.js
@@ -15,18 +15,16 @@ const app = express()
 
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'))
+// }
 
 app.use('/api/v1/transactions', transactions)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  )
-}
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
 
 const PORT = process.env.PORT || 5000
 
