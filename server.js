d131c2956493fdd9f5a088337499b8708c89e8dd
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-27 20:06:37 -0600

    Added/Moved public folder to root

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(express.json())
 
 app.use('/api/v1/transactions', transactions)
 
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 )
 
 const PORT = process.env.PORT || 5000
